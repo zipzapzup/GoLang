46b2654a2cd649fe7f25a5fe1bb6f913ac139078
LightBread <kjumino@hellozai.com>
2022-07-25 00:31:18 +1000

    golang data

---
+++ Ch1/go.mod
@@ -0,0 +1,3 @@
+module github.com/zipzapzup/ch1
+
+go 1.18
---
+++ Ch1/main.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	// integer literals
+
+	// base 10
+	var a int = 10
+	var b int = 20
+
+	// base 2 - binary
+	var c int = 0b01010
+
+	// base 8 - octal
+	// 024 octal is 20
+	var d int = 024
+
+	// base 16 - hexadecimal
+	// 14 hexa is 20
+	var e int = 0x14
+	fmt.Println("Base 10:", a+b, "Binary: ", c, "Octal: ", d, "Hexa:", e)
+
+	fmt.Println("Greetings")
+}
