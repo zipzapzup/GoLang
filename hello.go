1d1f7d4c9c661613033f4c81322a58177b794502
LightBread <kjumino@hellozai.com>
2022-07-25 00:32:02 +1000

    golang hello go

--- hello.go
+++ hello.go
@@ -3,6 +3,22 @@ package main
 import "fmt"
 
 func main() {
+
+	// integer literals
+
+	// base 10
+	var a int = 10
+	var b int = 20
+
+	// base 2 - binary
+	var c int = 0b01010
+
+	// base 8 - octal
+	// 024 octal is 20
+	var d int = 024
+
+	fmt.Println(a+b, "Binary: ", c, "Octal: ", d)
+
 	fmt.Println("Hello World")
 
 }
