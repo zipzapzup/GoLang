96a7f569bf7fd48491d99a7159d6cddcb8ebccea
zipzapzup <kelvin.jumino@gmail.com>
2022-09-15 20:34:54 +1000

    Fetch aka CURL

---
+++ GoProgrammingLanguage/Ch1/fetch.go
@@ -0,0 +1,29 @@
+// Fetch a URL and prints the content of the URL
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
+		if err != nil {
+			fmt.Println("URL not Found, Error:", err)
+			os.Exit(1)
+		}
+
+		b, err := ioutil.ReadAll(resp.Body)
+		resp.Body.Close()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+
+	}
+
+}
