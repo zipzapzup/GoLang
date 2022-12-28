7fb84bbcb2ba289aea94bd97668d224633454a06
ZipZapZup <kelvin.jumino@gmail.com>
2022-12-28 12:29:22 +1100

    Concurrency Golang, Channel and GoRoutines

--- GoProgrammingLanguage/Ch1/1.6FetchAll/fetch_concurrent.go
+++ GoProgrammingLanguage/Ch1/1.6FetchAll/fetch_concurrent.go
@@ -1,8 +1,47 @@
-main package
+package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
 
 func main() {
 
 	fmt.Println("Start of Execution Concurrent Programming - Fetch")
+	start := time.Now()
+	ch := make(chan string) // ch is the channel
 
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch) // start goroutine
+	}
 
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+
+	fmt.Printf("%.2fs elapsed\n", time.Since(start).Seconds())
+
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+
+	}
+
+	nbytes, err := io.Copy(ioutil.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+	}
+
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%.2fs %7d %s", secs, nbytes, url)
 }
