b5da1f6186f1b2a783d5e42e7cbdc730d91e833a
ZipZapZup <kelvin.jumino@gmail.com>
2022-12-29 14:25:55 +1100

    Name and Declarations

---
+++ GoProgrammingLanguage/Ch2/2.2Declarations/declare.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// package declaration
+const boilingPoint = 212.0
+
+func main() {
+	// function scope declaration within main function
+	var f = boilingPoint
+	var c = (f - 32) * 5 / 9
+
+	// details of fmt.Printf see: https://pkg.go.dev/fmt#example-package-Formats
+	// %v = default format value
+	// %t = boolean true or false
+	// %b = base 2
+	// %d = base 10
+	// %.2f = float points with 2 precision on default width
+	// %9.2f = width 9 with 2 precision point.
+
+	fmt.Printf("boiling point = %v, %v \n", f, c)
+
+}
