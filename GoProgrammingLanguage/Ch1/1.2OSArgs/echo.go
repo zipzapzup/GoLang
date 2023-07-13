32b0a717ffc4f3885b464f4b0029f10cd72e8bc7
Kelvin <kelvin.jumino@gmail.com>
2023-07-13 15:46:23 +1000

    Add notes on Time Complexity string operations

--- GoProgrammingLanguage/Ch1/1.2OSArgs/echo.go
+++ GoProgrammingLanguage/Ch1/1.2OSArgs/echo.go
@@ -15,3 +15,19 @@ func main() {
 
 // BigO O(n) Space - String Concatenation
 // BigO O(n) Time
+//
+// Important point, the string "s"
+// s = s + " " + os.Args[i]
+// is assigned a new value
+//
+// Loop 1 = " args1" |  10 bytes
+// Loop 2 = " args1 args2" | 24 bytes
+// Loop 3 = " args1 args2 args3" | 32 bytes
+//
+// Space Complexity: 10 bytes on 1st loop
+// Space Complexity: 34 bytes on the 2nd loop
+// Space Complexity: 76 bytes on the 3rd loop
+//
+//
+// string will occupy a quadratic space in proportion to the number of args
+// s will be garbage collected in due time
