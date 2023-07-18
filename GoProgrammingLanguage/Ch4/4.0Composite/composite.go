59b837b6c7dc109574d414705e494036cd9ca4f0
Kelvin <kelvin.jumino@gmail.com>
2023-07-18 15:47:55 +1000

    Composite types

---
+++ GoProgrammingLanguage/Ch4/4.0Composite/composite.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Composite types are the type that is made up from basic types
+	// arrays
+	// slices
+	// maps
+	// structs
+
+	fmt.Println("Composite Types")
+
+}
