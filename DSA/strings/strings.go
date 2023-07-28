dd0223e4a9006b5e79e98db7fad5c4ab041f9510
zipzapzup <kelvin.jumino@gmail.com>
2023-07-28 21:39:44 +1000

    strings interesting

--- DSA/strings/strings.go
+++ DSA/strings/strings.go
@@ -21,7 +21,60 @@ func main() {
 	z := "hello"
 	for i, v := range z {
 		fmt.Println(i, v, z[i])
+		if v == rune(' ') {
+			fmt.Println("A space here")
+		}
 	}
+
+	s1 := "apple pie 你好吧"
+	s2 := "i 你好吧"
+
+	fmt.Println(s1, s2)
+	fmt.Println(checkStrings(s1, s2))
+
+	stringbyte("Hello")
+
+}
+
+// check strings through loop
+func checkStrings(s, sub string) bool {
+	// to compare strings in golang
+	// you need to convert them to rune
+	// r is []rune type
+	r := []rune(sub)
+
+	// looping through strings give you runes
+	// UTF-8 value
+	// O(1) Space Complexity since it accessed the runes value
+	for _, v := range s {
+		if v == r[0] {
+			return true
+		}
+	}
+	return false
+}
+
+// check strings through function
+func checkStringsv2(s, sub string) bool {
+	// Contains will go through each character in strings
+	// O(n) Time Complexity
+	return strings.Contains(s, sub)
+}
+
+func stringLiteral() string {
+	return `Mary had a little lambs ?`
+}
+
+func stringbyte(s string) {
+	// index of string is accessible
+	// however it prints the bytes
+	fmt.Println("Running stringbyte: hello")
+	for index, value := range s {
+		fmt.Println(index, value)
+	}
+
+	// len(s) is also accessible
+	fmt.Println("length of s:", len(s))
 }
 =======
 package main
