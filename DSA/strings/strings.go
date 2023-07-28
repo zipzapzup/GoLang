27dbfc157a82f02ee2d073c25d1e306e0be42935
zipzapzup <kelvin.jumino@gmail.com>
2023-07-28 10:47:44 +1000

    strings

--- DSA/strings/strings.go
+++ DSA/strings/strings.go
@@ -8,9 +8,17 @@ import (
 func main() {
 	fmt.Println("Strings")
 
+	// strings.Join([]string, sep) is O(N) ST Operation
 	a := []string{"ab", "ba"}
 	fmt.Println(a)
 
 	joined := strings.Join(a, "")
 	fmt.Println(joined)
+
+	fmt.Println(5381 * 33)
+
+	z := "hello"
+	for _, i := range z {
+		fmt.Println(int(i), i)
+	}
 }
